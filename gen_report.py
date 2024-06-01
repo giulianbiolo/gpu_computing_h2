8dcb658b8f44f065b78e4cb10863267aa79d6703
Giulian Biolo <giulianbiolo@gmail.com>
2024-06-01 13:59:27 +0200

    Added memory throughput graph generation

--- gen_report.py
+++ gen_report.py
@@ -28,6 +28,7 @@ Output Format:
 > Graph with N on x-axis and KThroughput on y-axis ( With 3 different colored lines, one for each method, with legend )      # shows throughput of the kernel only
 > Graph with N on x-axis and OpThroughput on y-axis ( With 3 different colored lines, one for each method, with legend )     # shows throughput of the operations
 > Graph with N on x-axis and (OpTime - KTime) on y-axis ( With 3 different colored lines, one for each method, with legend ) # shows time taken only by memory operations
+> Graph with N on x-axis and (N*N*4*2) / (OpTime - KTime) on y-axis ( With 3 different colored lines, one for each method, with legend ) # shows memory throughput
 Save these graphs in the folder 'graphs' under the names 'kthroughput.png', 'opthroughput.png', 'memtime.png'
 '''
 
@@ -109,6 +110,35 @@ def plot_memtime(data: list[GraphData]) -> None:
     plt.savefig(f'graphs/memtime.png')
     plt.show()
 
+def plot_memthroughput(data: list[GraphData]) -> None:
+    fig, ax = plt.subplots()
+    fig.set_size_inches(19.20, 10.80)
+    labels = ['Naive Transpose', 'Block Transpose With Bank Conflicts', 'Block Transpose Coalesced']
+    for i in range(len(data)):
+        # optime - ktime = memtime, we want memthroughput which is N*N*4*2 / memtime
+        mem_throughput = [(data[i].N[j] * data[i].N[j] * 4 * 2) / (data[i].OpTime[j] - data[i].KTime[j]) for j in range(len(data[i].N))]
+        ax.plot(data[i].N, mem_throughput, label = labels[i])
+    ax.set_title('Memory Throughput over Matrix Size')
+    ax.set_xlabel('Matrix Size NxN')
+    ax.set_ylabel('Memory Throughput [ GB/s ]')
+    ax.legend()
+    plt.savefig(f'graphs/memthroughput.png')
+    plt.show()
+    # also plot it only to N[:5]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(19.20, 10.80)
+    labels = ['Naive Transpose', 'Block Transpose With Bank Conflicts', 'Block Transpose Coalesced']
+    for i in range(len(data)):
+        # optime - ktime = memtime, we want memthroughput which is N*N*4*2 / memtime
+        mem_throughput = [(data[i].N[j] * data[i].N[j] * 4 * 2) / (data[i].OpTime[j] - data[i].KTime[j]) for j in range(len(data[i].N))]
+        ax.plot(data[i].N[:5], mem_throughput[:5], label = labels[i])
+    ax.set_title('Memory Throughput over Matrix Size (Only up to 1024)')
+    ax.set_xlabel('Matrix Size NxN')
+    ax.set_ylabel('Memory Throughput [ GB/s ]')
+    ax.legend()
+    plt.savefig(f'graphs/memthroughput_upto1024.png')
+    plt.show()
+
 def main(filename: str) -> None:
     data: list[GraphData] = load_data(filename)
     print(data)
@@ -117,6 +147,7 @@ def main(filename: str) -> None:
     plot_kthroughput(data)
     plot_opthroughput(data)
     plot_memtime(data)
+    plot_memthroughput(data)
 
 
 if __name__ == '__main__':
